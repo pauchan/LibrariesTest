4b64ae997bbba757e2ce4fd1a5461fa53a484d06
Pawel Rusin <pawel-rusin@cookpad.com>
2016-12-03 15:48:05 +0900

    Initial Commit

---
+++ MerryChristmas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MerryChristmas
+//
+//  Created by pawel-rusin on 12/3/16.
+//  Copyright © 2016 pawel-rusin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
